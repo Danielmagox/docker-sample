01e282f0453d4284d9327696f7d30e81c68e5ef8
Danielmagox <daniel1305972@gmail.com>
2020-10-20 21:33:50 +0100

    docker compose

---
+++ nginx/nginx-1.19.dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.19.3-alpine
+RUN touch /fichero.txt
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker build . -t debian-nginx:1.0
-docker run -it -v $(pwd)/html:/var/www/html -p 8080:80 debian-nginx:1.0
+docker compose build && docker compose up
